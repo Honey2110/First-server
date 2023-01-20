21035738c7ea7f1aea71d9d2e507aa592f9be43a
Honey2110 <swapnil21.sk@gmail.com>
2023-01-21 01:45:40 +0530

    Created and learned about github more

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const http = require("http");
+
 const port = 8081; // local port no
 
 http
@@ -8,8 +10,7 @@ http
     res.end();
   })
   .listen(port, () => {
-    // callback function
+    // callback function 
     console.log(`Nodejs server started on port ${port}`);
   });
 
-// http://localhost:8081
