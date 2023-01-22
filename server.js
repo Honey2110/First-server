f156769dc79ab79ee1eee500a8b4c3a19f292f7e
Honey2110 <swapnil21.sk@gmail.com>
2023-01-22 20:54:44 +0530

    learned more about methods .on and csr and ssr

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const http = require("http");
+const port = 8081;
 
-const port = 8081; // local port no
+const toDoList = ["Complete Node Byte", "Play Cricket"];
 
-
-const todolist = ["Complete Node Byte", "Play Cricket"];
 http
   .createServer((req, res) => {
-    // callback function
     const { method, url } = req;
 
     if (url === "/todos") {
       if (method === "GET") {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(todolist.toString());
+        res.write(toDoList.toString());
       } else if (method === "POST") {
         let body = "";
         req
@@ -21,23 +19,51 @@ http
           })
           .on("data", (chunk) => {
             body += chunk;
-            console.log(chunk);
           })
           .on("end", () => {
             body = JSON.parse(body);
-            console.log(body);
+            let newToDo = toDoList;
+            newToDo.push(body.item);
+            console.log(newToDo);
+            res.writeHead(201);
+          });
+      } else if (method === "DELETE") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
           })
+          .on("data", (chunk) => {
+            body += chunk;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let deleteThis = body.item;
+
+            // for (let i = 0; i < toDoList.length; i++) {
+            //   if (toDoList[i] === deleteThis) {
+            //     toDoList.splice(i, 1);
+            //     break;
+            //   }
+            // }
+
+            toDoList.find((element, index) => {
+              if (element === deleteThis) {
+                toDoList.splice(index, 1);
+              }
+            });
+
+            res.writeHead(204);
+          });
       } else {
-        res.writeHead(404);
+        res.writeHead(501);
       }
     } else {
-      res.writeHead(505);
+      res.writeHead(404);
     }
 
     res.end();
   })
   .listen(port, () => {
-    // callback function 
     console.log(`Nodejs server started on port ${port}`);
   });
-
